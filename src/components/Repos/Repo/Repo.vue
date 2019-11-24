404dcfa673e67d9b22c293f52dc60422ae6ba380
Hamza NOUINOU <nouinou@Hamzas-iMac.local>
2019-11-24 14:42:50 +0100

    Axios + Fetching, filtering and displaying Repos

--- src/components/Repos/Repo/Repo.vue
+++ src/components/Repos/Repo/Repo.vue
@@ -1,11 +1,34 @@
 <template>
     <div class="Repo">
-            Single Repo Component is Working
+        <img class="repo_avatar" :src=owner_avatar alt="The avatar image of the repository's owner">
+        <div class="repo_info">
+            <h2 class="name"><a :href="url" target="_blank">{{name}}</a></h2>
+            <p class="description">{{description}}</p> 
+            <div class="stars_issues">
+                <span class="stars">{{stars}} star</span> 
+                <span class="issues">{{issues}} issue</span> 
+            </div>
+            <p>Created at {{created_at.slice(0,10)}} by 
+                <strong><a :href="owner_url" target="_blank"> @{{owner_login}}</a></strong>
+            </p> 
+        </div>
+            <hr>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'name'
+    name: 'name',
+    props: {
+        name: String,
+        description: String,
+        stars: Number,
+        issues: Number,
+        owner_login: String,
+        owner_avatar: String,
+        owner_url: String,
+        url: String,
+        created_at: String
+    }
 }
 </script>
--- src/components/Repos/Repos.vue
+++ src/components/Repos/Repos.vue
@@ -2,15 +2,34 @@
     <div id="Repos" >
         <div id="repo_date_filter">
             <!-- 2008-02-08 is Github creation date  -->
-            <input type="date" v-model="date" min="2008-02-08" :max="today" />
+            <input 
+                type="date" 
+                v-model="date" 
+                min="2008-02-08" 
+                :max="today"
+                v-on:change=update()
+                 />
         </div>
         <div id="repos_container">
-            <Repo v-for="repo in repos" v-bind:key="repo.id" />
+            <Repo 
+                v-for="repo in repos" 
+                v-bind:key="repo.id"
+                :name ="repo.name" 
+                :description="repo.description" 
+                :stars="repo.stargazers_count"
+                :issues="repo.open_issues_count"
+                :owner_login="repo.owner.login"
+                :owner_avatar="repo.owner.avatar_url"
+                :owner_url="repo.owner.html_url"
+                :url="repo.html_url"
+                :created_at="repo.created_at"
+                 />
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Repo from './Repo/Repo.vue';
 
 export default {
@@ -20,12 +39,30 @@ export default {
     },
     data(){
         return {
+            source: 'https://api.github.com/search/repositories?q=created:>',
+            apiURl: 'https://api.github.com/search/repositories?q=created:>',
             today: this.getTodayDate(),
             date: this.getPastMonth(),
+            sort: '&sort=stars',
+            order: '&order=desc',
             repos: [],
         }
     },
+    mounted(){
+        this.apiURl += this.date + this.sort + this.order;
+        this.fetch();
+    },
     methods: {
+        fetch(){
+            axios.get(this.apiURl).then(res => {
+                this.repos = res.data.items;
+            });
+        },
+        update(){
+            //changing date and fetching again
+            this.apiURl = this.source + this.date +  this.sort + this.order;
+            this.fetch();
+        },
         getTodayDate: function(){
             return this.formateDate(new Date());
         },
