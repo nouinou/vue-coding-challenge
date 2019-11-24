f6140b8bb95b141edba2e1d2a02e2bcd42deab8d
Hamza NOUINOU <nouinou@Hamzas-iMac.local>
2019-11-24 13:56:31 +0100

    Repos C - Date  input + repos v-for

--- src/components/Repos/Repos.vue
+++ src/components/Repos/Repos.vue
@@ -1,8 +1,11 @@
 <template>
-    <div id="Repos">
+    <div id="Repos" >
+        <div id="repo_date_filter">
+            <!-- 2008-02-08 is Github creation date  -->
+            <input type="date" v-model="date" min="2008-02-08" :max="today" />
+        </div>
         <div id="repos_container">
-            Repos Component is Working
-            <Repo />
+            <Repo v-for="repo in repos" v-bind:key="repo.id" />
         </div>
     </div>
 </template>
@@ -14,6 +17,37 @@ export default {
     name: 'Repos',
     components: {
         Repo
+    },
+    data(){
+        return {
+            today: this.getTodayDate(),
+            date: this.getPastMonth(),
+            repos: [],
+        }
+    },
+    methods: {
+        getTodayDate: function(){
+            return this.formateDate(new Date());
+        },
+        getPastMonth: function(){
+            let today = new Date();
+            // Subsctracting 30 days from today's date
+            let aMonthBefore = today.setDate(today.getDate() - 30);
+            return this.formateDate(aMonthBefore);
+        },
+        formateDate: function(date) { // Format date to yyyy-mm-dd
+            var d = new Date(date),
+                month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
+
+            if (month.length < 2) 
+                month = '0' + month;
+            if (day.length < 2) 
+                day = '0' + day;
+
+            return [year, month, day].join('-');
+        }
     }
 }
 </script>
